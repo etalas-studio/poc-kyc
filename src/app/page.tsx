4bc304e000ff0937771d4b73deb684a94b35879e
hanifski <h@etalas.com>
2025-09-19 20:33:00 +0700

    redirection fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-"use client";
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push("/login");
-  }, []);
-  return <div className=""></div>;
+  redirect("/login");
 }
